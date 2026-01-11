338e5316cc7f90b3f3ff68dcc74e3582681a46ea
ER-EPR <38782737+ER-EPR@users.noreply.github.com>
2026-01-11 19:57:31 +0800

    Add command to start Caddy server

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ hysteria server -c $1 &
 # Start the second process
 xray run -config $2 &
 
+# Start the third process
+caddy run --config $3 --adapter caddyfile &
+
 # Wait for any process to exit
 wait -n
 
